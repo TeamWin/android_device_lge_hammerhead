5f9b25e0a8128cee9efb5ad0b7c3b0c1c8e213f3
Dees Troy <dees_troy@teamw.in>
2013-11-05 10:27:37 +0100

    hammerhead: TWRP-ify
    Change-Id: I8dd3a05b98c1e3c6db46a52f7e33c7a5aefd7d1b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,3 +109,17 @@ HAVE_ADRENO_SOURCE:= false
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 
 -include vendor/lge/hammerhead/BoardConfigVendor.mk
+
+#TWRP
+DEVICE_RESOLUTION := 1080x1920
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := false
+TW_INCLUDE_JB_CRYPTO := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
+TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.160/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_NO_SCREEN_TIMEOUT := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
