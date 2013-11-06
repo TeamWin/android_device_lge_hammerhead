6faee77ae83ba641dfc4531ce9a0cffa6badc337
Dees Troy <dees_troy@teamw.in>
2013-11-06 18:15:29 +0000

    Add EFS backup/restore, larger font, no screen timeout

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,4 +120,5 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
 TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.160/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
-
+TW_NO_SCREEN_TIMEOUT := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
