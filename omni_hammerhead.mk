c855386a2fd01df61f65e441cc8928fe6459fd1a
xplodwild <xplod@ouverta.fr>
2013-11-11 23:14:40 +0100

    kernel: Use a separate path
    This avoids path conflicts/checkout error when multiple device
    use the same original google_msm kernel repository (e.g. if you
    build both flo and hammerhead)
    
    Change-Id: Ie4f1e801fdb6c2176abbd42a4878477139edeaaa

--- omni_hammerhead.mk
+++ omni_hammerhead.mk
@@ -36,7 +36,7 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
 # Kernel inline build
-TARGET_KERNEL_SOURCE := kernel/google/msm
+TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := custom_hammerhead_defconfig
 TARGET_VARIANT_CONFIG := custom_hammerhead_defconfig
 TARGET_SELINUX_CONFIG := custom_hammerhead_defconfig
