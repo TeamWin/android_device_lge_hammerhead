40728bb2faecb051aae2d02f4084a0daebd696a1
XpLoDWilD <me@xplod.fr>
2014-11-13 16:39:02 +0000

    Remove unsupported values for now
    Change-Id: I3bddaec70c80a5213191e318eb9ff25ae4e070ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ DEVICE_RESOLUTION := 1080x1920
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_NO_USB_STORAGE := false
-TW_INCLUDE_JB_CRYPTO := true
+#TW_INCLUDE_JB_CRYPTO := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
